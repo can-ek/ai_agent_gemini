777d86a56f95cd9dfa077c9b24023c20294209d0
Canek <canek@pop-os.local>
2025-07-05 18:56:56 -0700

    Setting up project to interact with Gemini

---
+++ README.md
@@ -0,0 +1,31 @@
+### Create project with UV
+
+```
+uv init your-project-name
+cd your-project-name
+```
+
+### Create Virtual environment
+
+```
+uv venv
+```
+
+### Activate Virtaul environment
+
+```
+source .venv/bin/activate
+```
+
+### Add dependencies with UV
+
+```
+uv add google-genai==1.12.1
+uv add python-dotenv==1.1.0
+```
+
+### Run project
+
+```
+uv run main.py
+```
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from ai-agent-gemini!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+content_response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
 
-if __name__ == "__main__":
-    main()
+print(content_response.text)
+print(f"Prompt tokens: {content_response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {content_response.usage_metadata.candidates_token_count}")
