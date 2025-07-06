b65bff79cc89316d9bbee81fe380f01d12f8b4f1
can-ek <canekcambrayherrera@gmail.com>
2025-07-05 19:03:47 -0700

    Supporting verbose argument

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-if len(sys.argv) != 2:
+
+if len(sys.argv) < 2:
   print("Error: Missing prompt argument pos 1")
   exit(1)
 
 prompt = sys.argv[1]
-load_dotenv()
+verbose = '--verbose' in sys.argv 
 
+messages = [ types.Content(role="user", parts=[types.Part(text=prompt)]), ]
+load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
-
-content_response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-content_response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+content_response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
 print(content_response.text)
-print(f"Prompt tokens: {content_response.usage_metadata.prompt_token_count}")
+
+if "--verbose" in sys.argv:
+  print(f"User prompt: {prompt}")
+  print(f"Prompt tokens: {content_response.usage_metadata.prompt_token_count}")
+  print(f"Response tokens: {content_response.usage_metadata.candidates_token_count}")
