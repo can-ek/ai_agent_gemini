82e395f4f145b17bc4e4ad9775204b294f0b95ae
Canek <canekcambrayherrera@gmail.com>
2025-07-12 19:05:27 -0700

    Create loop for the agent

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from functions.call_function import call_function, available_functions
+from config import MAX_TRIES
 
 if len(sys.argv) < 2:
   print("Error: Missing prompt argument pos 1")
@@ -28,22 +29,34 @@ All paths you provide should be relative to the working directory. You do not ne
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
-content_response = client.models.generate_content(
-  model="gemini-2.0-flash-001",
-  contents=messages,
-  config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
 
-if content_response.function_calls:
-  for f_call in content_response.function_calls:
-    result = call_function(f_call, verbose=verbose)
-    
-    if not result.parts[0].function_response.response:
-      raise Exception('Result missing parts')
-    
-    if verbose:
-      print(f"-> {result.parts[0].function_response.response}")
-else:
-  print(content_response.text)
+retries = 0
+
+while retries < MAX_TRIES:
+  content_response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=messages,
+    config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
+
+  for candidate in content_response.candidates:
+    messages.append(candidate.content)
+
+  if content_response.function_calls:
+    for f_call in content_response.function_calls:
+      result = call_function(f_call, verbose=verbose)
+      
+      if not result.parts[0].function_response.response:
+        raise Exception('Result missing parts')
+      
+      if verbose:
+        print(f"-> {result.parts[0].function_response.response}")
+
+      messages.append(result)
+      retries += 1
+  else:
+    print(content_response.text)
+    break
+
 
 if verbose:
   print(f"User prompt: {prompt}")
